c0710194c9e89ec6de735ff823d683b9fc5a25ff
Nicolai Glock <niglit01@hs-esslingen.de>
2024-05-23 11:44:00 +0200

    add Frontend

---
+++ frontend/jodelui/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div className="jodel"></div>
+        <p>
+        Jodel by Linus Macha & Nicolai Glock 
+        </p>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
